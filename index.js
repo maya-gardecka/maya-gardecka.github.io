2cb12f4244b4c06937615917212080b3f1f9e46d
Maya Gardecka <97481772+maya-gardecka@users.noreply.github.com>
2025-06-03 10:23:48 -0700

    basic project structure and setup

---
+++ index.js
@@ -0,0 +1,34 @@
+const mainContainer = document.getElementById("main-container");
+
+const pages = {
+  "Helpdesk Tools": [
+    "helpdesk/helpdesk.html",
+    "A dynamic and responsive site built using HTML, CSS, and JavaScript",
+    "Set tools for the helpdesk employee",
+  ],
+  // test2: ["z", "y", "x"],
+};
+
+function loadToolButtons() {
+  const toolContainer = document.getElementById("tool-selection");
+  const ol = document.createElement("ol");
+  Object.entries(pages).forEach(([toolName, toolInfo]) => {
+    const main_li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = toolInfo[0];
+    a.textContent = toolName;
+    a.target = "_blank";
+    main_li.appendChild(a);
+    const ul = document.createElement("ul");
+    for (const subItem of toolInfo.slice(1)) {
+      const li = document.createElement("li");
+      li.textContent = subItem;
+      ul.appendChild(li);
+    }
+    main_li.appendChild(ul);
+    ol.appendChild(main_li);
+    toolContainer.appendChild(ol);
+  });
+}
+
+loadToolButtons();
